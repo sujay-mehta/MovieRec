0e080f6a2609f8ed83f800e69af1a22ffcf92eae
Rahul Mehta <rkmehta@usc.edu>
2024-09-03 21:40:24 -0700

    Initial Commit

---
+++ MovieIT/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MovieIT
+//
+//  Created by Rahul Mehta on 9/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MovieIT/MovieITApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieITApp.swift
+//  MovieIT
+//
+//  Created by Rahul Mehta on 9/3/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
